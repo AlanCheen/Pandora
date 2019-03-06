33c8a93fc0107ef4b6f59cf126301a725270ebef
mingjue <zhening.czn@alibaba-inc.com>
2019-03-06 16:29:43 +0800

    add collections

---
+++ pandora/src/main/java/me/yifeiyuan/pandora/collection/CollectionUtils.java
@@ -0,0 +1,24 @@
+package me.yifeiyuan.pandora.collection;
+
+import android.support.annotation.Nullable;
+
+import java.util.Collection;
+import java.util.Map;
+
+/**
+ * Created by 程序亦非猿 on 2019/3/6.
+ */
+public final class CollectionUtils {
+
+    private CollectionUtils() {
+        throw new AssertionError("No instances.");
+    }
+
+    public static boolean isEmpty(@Nullable final Collection<?> collection) {
+        return collection == null || collection.isEmpty();
+    }
+
+    public static <K, V, M extends Map<K, V>> boolean isEmpty(@Nullable final M map) {
+        return map == null || map.isEmpty();
+    }
+}
---
+++ pandora/src/main/java/me/yifeiyuan/pandora/ui/ToastUtils.java
@@ -0,0 +1,52 @@
+package me.yifeiyuan.pandora.ui;
+
+import android.widget.Toast;
+
+import me.yifeiyuan.pandora.app.AppUtils;
+import me.yifeiyuan.pandora.app.ApplicationUtils;
+import me.yifeiyuan.pandora.thread.MainThread;
+
+/**
+ * Created by 程序亦非猿 on 2019/3/6.
+ */
+public final class ToastUtils {
+
+    private ToastUtils() {
+        throw new AssertionError("No instances.");
+    }
+
+    private static final Toast sToast;
+
+    static {
+        sToast = Toast.makeText(ApplicationUtils.getApplication(), "", Toast.LENGTH_LONG);
+    }
+
+    /**
+     * 弹 Toast , 复用一个单例，解决多次弹 Toast 的问题。
+     * @param msg
+     */
+    public static void show(final CharSequence msg) {
+        MainThread.post(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    sToast.cancel();
+                    sToast.setText(msg);
+                    sToast.show();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /**
+     * 在 App 处于 Debug 的环境下弹 Toast。
+     * @param msg
+     */
+    public static void debug(final CharSequence msg) {
+        if (AppUtils.isAppDebugable()) {
+            show(msg);
+        }
+    }
+}
