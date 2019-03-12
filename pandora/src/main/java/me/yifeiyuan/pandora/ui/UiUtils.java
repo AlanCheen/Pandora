430c79007a08d1712413e4afb9c2996f270cdc8b
mingjue <zhening.czn@alibaba-inc.com>
2019-03-12 18:10:25 +0800

    remove methods

--- pandora/src/main/java/me/yifeiyuan/pandora/ui/UiUtils.java
+++ pandora/src/main/java/me/yifeiyuan/pandora/ui/UiUtils.java
@@ -1,15 +1,9 @@
 package me.yifeiyuan.pandora.ui;
 
-import android.annotation.TargetApi;
-import android.app.Activity;
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.Rect;
-import android.os.Build;
 import android.support.annotation.NonNull;
 import android.util.DisplayMetrics;
 import android.util.TypedValue;
-import android.view.View;
 import android.view.WindowManager;
 
 import me.yifeiyuan.pandora.app.ApplicationUtils;
@@ -86,86 +80,5 @@ public final class UiUtils {
         }
 
         return result;
-    }
-
-    /**
-     * 获取toolbar的高度
-     *
-     * @param context
-     *
-     * @return 默认为0
-     */
-    public static int getToolbarHeight(@NonNull Context context) {
-        TypedValue typedValue = new TypedValue();
-
-        if (context.getTheme().resolveAttribute(android.R.attr.actionBarSize, typedValue, true)) {
-            return TypedValue.complexToDimensionPixelSize(typedValue.data, context.getResources().getDisplayMetrics());
-        }
-
-        return 0;
-    }
-
-    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
-    private int getSoftButtonsBarHeight(@NonNull Context context) {
-
-        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
-
-        DisplayMetrics metrics = new DisplayMetrics();
-
-        wm.getDefaultDisplay().getMetrics(metrics);
-        int usableHeight = metrics.heightPixels;
-
-        wm.getDefaultDisplay().getRealMetrics(metrics);
-        int realHeight = metrics.heightPixels;
-
-        if (realHeight > usableHeight) {
-            return realHeight - usableHeight;
-        } else {
-            return 0;
-        }
-    }
-
-    /**
-     * 获取当前屏幕截图 包含状态栏
-     *
-     * @param activity
-     *
-     * @return
-     */
-    public static Bitmap snapShotWithStatusBar(@NonNull Activity activity) {
-        View view = activity.getWindow().getDecorView();
-        view.setDrawingCacheEnabled(true);
-        view.buildDrawingCache();
-        Bitmap bmp = view.getDrawingCache();
-        int width = getScreenWidth(activity);
-        int height = getScreenHeight(activity);
-        Bitmap bp = null;
-        bp = Bitmap.createBitmap(bmp, 0, 0, width, height);
-        view.destroyDrawingCache();
-        return bp;
-    }
-
-    /**
-     * 获取当前屏幕截图 但不包含状态栏
-     *
-     * @param activity
-     *
-     * @return
-     */
-    public static Bitmap snapShotWithoutStatusBar(@NonNull Activity activity) {
-        View view = activity.getWindow().getDecorView();
-        view.setDrawingCacheEnabled(true);
-        view.buildDrawingCache();
-        Bitmap bmp = view.getDrawingCache();
-        Rect frame = new Rect();
-        view.getWindowVisibleDisplayFrame(frame);
-        int statusBarHeight = frame.top;
-
-        int width = getScreenWidth(activity);
-        int height = getScreenHeight(activity);
-        Bitmap bp = null;
-        bp = Bitmap.createBitmap(bmp, 0, statusBarHeight, width, height - statusBarHeight);
-        view.destroyDrawingCache();
-        return bp;
     }
 }
