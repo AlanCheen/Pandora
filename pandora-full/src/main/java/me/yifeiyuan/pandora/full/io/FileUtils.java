1a1362e3f2d3b9b67dd59d18d3cac77dd5dac631
mingjue <zhening.czn@alibaba-inc.com>
2019-07-10 14:04:42 +0800

    update fileutils

--- pandora-full/src/main/java/me/yifeiyuan/pandora/full/io/FileUtils.java
+++ pandora-full/src/main/java/me/yifeiyuan/pandora/full/io/FileUtils.java
@@ -1,5 +1,6 @@
 package me.yifeiyuan.pandora.full.io;
 
+import java.io.File;
 /**
  * Created by 程序亦非猿 on 2019/3/5.
  */
@@ -7,5 +8,14 @@ public final class FileUtils {
 
     private FileUtils() {
         throw new AssertionError("No instances.");
+    }
+
+    public static boolean deleteFile(String path) {
+        File file = new File(path);
+        return file.delete();
+    }
+
+    public static boolean deleteFile(File file) {
+        return file.delete();
     }
 }
