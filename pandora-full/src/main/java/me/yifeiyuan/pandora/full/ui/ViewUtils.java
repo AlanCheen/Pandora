a3406e55732a76fd65fad48b6735eea089169478
mingjue <zhening.czn@alibaba-inc.com>
2019-08-29 15:10:12 +0800

    add pandora-full/src/main/java/me/yifeiyuan/pandora/full/ui/ViewUtils.java

---
+++ pandora-full/src/main/java/me/yifeiyuan/pandora/full/ui/ViewUtils.java
@@ -0,0 +1,52 @@
+package me.yifeiyuan.pandora.full.ui;
+
+import android.graphics.Rect;
+import android.view.TouchDelegate;
+import android.view.View;
+
+import me.yifeiyuan.pandora.UiUtils;
+/**
+ * Created by 程序亦非猿 on 2019-08-29.
+ */
+public final class ViewUtils {
+
+    private ViewUtils() {
+        //no instance
+        throw new AssertionError("No instances.");
+    }
+
+    /**
+     * 扩展一个 View 的点击范围
+     *
+     * 通常扩展点击范围可以用 padding 来处理，如果 padding 不适合则可以使用该方法。
+     *
+     * @param target，你想要扩展点击区域的 View
+     * @param areaInDp，要扩展的大小，单位为 dp
+     */
+    public static void expandClickArea(final View target, final int areaInDp) {
+
+        if (target.getParent() instanceof View) {
+
+            final View parent = (View) target.getParent();
+
+            parent.post(new Runnable() {
+                @Override
+                public void run() {
+                    Rect bounds = new Rect();
+                    target.getHitRect(bounds);
+
+                    int v = (int) UiUtils.dp2px(areaInDp);
+
+                    bounds.left -= v;
+                    bounds.top -= v;
+                    bounds.right += v;
+                    bounds.bottom += v;
+
+                    TouchDelegate delegate = new TouchDelegate(bounds, target);
+                    parent.setTouchDelegate(delegate);
+                }
+            });
+        }
+    }
+
+}
